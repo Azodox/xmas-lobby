6be7a52547f405b39786b4e3f43f0094a616cf2f
Azodox_ <likemoi99@gmail.com>
2022-04-18 14:28:00 +0200

    Import base plugin.

---
+++ src/main/java/fr/olten/xmas/Lobby.java
@@ -0,0 +1,12 @@
+package fr.olten.xmas;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        getLogger().info("Enabled!");
+    }
+}
