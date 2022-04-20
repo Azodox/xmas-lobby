5de366a9b77bafa5c7d49f2b1e4f2efa1781140b
Azodox_ <likemoi99@gmail.com>
2022-04-20 02:29:45 +0200

    Adding first server's protection.

---
+++ src/main/java/fr/olten/xmas/listener/ProtectionListener.java
@@ -0,0 +1,20 @@
+package fr.olten.xmas.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class ProtectionListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBlockBreak(BlockBreakEvent event){
+        event.setCancelled(!event.getPlayer().hasPermission("xmas.protect.break"));
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBlockPlace(BlockPlaceEvent event){
+        event.setCancelled(!event.getPlayer().hasPermission("xmas.protect.place"));
+    }
+}
