4cdfbc8d5f64706520864b555a3c6c9c27497fe7
Virtuaal_ <mattiro@live.fr>
2022-04-21 23:47:22 -0400

    Main class now contains a getRoulette().

--- src/main/java/fr/olten/xmas/Lobby.java
+++ src/main/java/fr/olten/xmas/Lobby.java
@@ -13,6 +13,8 @@ import fr.olten.xmas.listener.rank.RankChangedListener;
 import fr.olten.xmas.listener.sign.SignBreakListener;
 import fr.olten.xmas.listener.sign.SignInteractListener;
 import fr.olten.xmas.manager.TeamNameTagManager;
+import fr.olten.xmas.rewards.RewardHandler;
+import fr.olten.xmas.roulette.Roulette;
 import net.valneas.account.rank.RankUnit;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -27,6 +29,7 @@ public class Lobby extends JavaPlugin {
 
     private final Set<Achievement> achievements = new HashSet<>();
     private Carousel carousel;
+    private Roulette roulette;
 
     @Override
     public void onEnable() {
@@ -34,6 +37,17 @@ public class Lobby extends JavaPlugin {
         this.carousel = new Carousel(new Location(DEFAULT_WORLD, -483.0, 102.0, -461.0),
                 new Location(DEFAULT_WORLD, -494.0, 100.0, -461, (float) -90.0, (float) 0.0),
                 (Sign) DEFAULT_WORLD.getBlockAt(new Location(DEFAULT_WORLD, -492.0, 100.0, -461.0)).getState());
+
+        this.roulette = new Roulette((Sign) DEFAULT_WORLD.getBlockAt(new Location(DEFAULT_WORLD, -560.0, 102.0, -424.0)).getState(),
+                                Arrays.asList(new Location(DEFAULT_WORLD, -558.0, 104, -423),new Location(DEFAULT_WORLD, -559.0, 104.0, -423.0),
+                                             new Location(DEFAULT_WORLD, -560, 104.0, -423.0),new Location(DEFAULT_WORLD, -561.0, 104.0, -423.0),
+                                             new Location(DEFAULT_WORLD, -562.0, 104.0, -423.0),new Location(DEFAULT_WORLD, -562.0, 103.0, -423.0),
+                                             new Location(DEFAULT_WORLD, -562.0, 102.0, -423.0),new Location(DEFAULT_WORLD, -562.0, 101.0, -423.0),
+                                             new Location(DEFAULT_WORLD, -562.0, 100.0, -423.0), new Location(DEFAULT_WORLD, -561.0, 100.0, -423.0),
+                                             new Location(DEFAULT_WORLD, -560.0, 100.0, -423.0), new Location(DEFAULT_WORLD, -559.0, 100.0, -423.0),
+                                             new Location(DEFAULT_WORLD, -558.0, 100.0, -423.0),new Location(DEFAULT_WORLD, -558.0, 101.0, -423.0),
+                                             new Location(DEFAULT_WORLD, -558.0, 102.0, -423.0),new Location(DEFAULT_WORLD, -558.0, 103.0, -423.0),
+                                             new Location(DEFAULT_WORLD, -558.0, 104.0, -423.0)),this);
 
         new Engine(this).runTaskTimer(this, 0L, 1L);
 
@@ -67,5 +81,8 @@ public class Lobby extends JavaPlugin {
 
     public Carousel getCarousel() {
         return carousel;
+    }
+    public Roulette getRoulette() {
+        return roulette;
     }
 }
