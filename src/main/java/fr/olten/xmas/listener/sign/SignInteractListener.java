8acb928227ca25b33a4596ad2eb3f55bb23ea850
Virtuaal_ <mattiro@live.fr>
2022-04-22 16:26:18 -0400

    Made it so now it opens a menu when the roulette sign is clicked.

--- src/main/java/fr/olten/xmas/listener/sign/SignInteractListener.java
+++ src/main/java/fr/olten/xmas/listener/sign/SignInteractListener.java
@@ -1,10 +1,21 @@
 package fr.olten.xmas.listener.sign;
 
 import fr.olten.xmas.Lobby;
+import fr.olten.xmas.roulette.keys.KeyDAL;
+import fr.olten.xmas.roulette.keys.KeyManager;
+import fr.olten.xmas.roulette.keys.KeyTypes;
+import fr.olten.xmas.utils.ScrollerInventory;
+import net.kyori.adventure.text.Component;
+import org.bson.Document;
+import org.bukkit.ChatColor;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
 
 public class SignInteractListener implements Listener {
 
@@ -24,10 +35,30 @@ public class SignInteractListener implements Listener {
                 }
 
                 if(sign.getLocation().equals(lobby.getRoulette().getStartSign().getLocation())){
-                    lobby.getRoulette().manager().setPlayer(event.getPlayer());
-                    lobby.getRoulette().manager().start();
+                    openRouletteMenu(event.getPlayer());
                 }
             }
         }
+    }
+
+    // Open the roulette inventory to the player.
+    private void openRouletteMenu(Player player){
+
+        var items = new ArrayList<ItemStack>();
+
+        KeyDAL keyDAL = new KeyDAL(lobby);
+        Document doc = keyDAL.getKeys(player);
+        KeyManager keyManager = new KeyManager(lobby);
+
+        for (KeyTypes key : KeyTypes.values()) {
+            int i = doc.getInteger(key.name().toLowerCase());
+            if(i > 0){
+                ItemStack stack = keyManager.getKey(key).getItem();
+                stack.setAmount(i);
+                items.add(stack);
+            }
+        }
+
+        new ScrollerInventory(items, Component.text("Roulette"), player);
     }
 }
