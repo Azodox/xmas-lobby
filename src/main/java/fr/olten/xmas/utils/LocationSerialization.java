01de54740d313a5b536ad490c61834b0d9802d53
Azodox_ <likemoi99@gmail.com>
2022-04-25 23:45:30 +0200

    Adding a serializer for Location that parses from a String to a Location or from a Location to a String

---
+++ src/main/java/fr/olten/xmas/utils/LocationSerialization.java
@@ -0,0 +1,24 @@
+package fr.olten.xmas.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class LocationSerialization {
+
+    // Convert string to location.
+    public static Location deserialize(String str){
+        String[] str2loc = str.split(":");
+        Location loc = new Location(Bukkit.getServer().getWorld(str2loc[0]),0,0,0);
+        loc.setX(Double.parseDouble(str2loc[1]));
+        loc.setY(Double.parseDouble(str2loc[2]));
+        loc.setZ(Double.parseDouble(str2loc[3]));
+        loc.setPitch(Float.parseFloat(str2loc[4]));
+        loc.setYaw(Float.parseFloat(str2loc[4]));
+        return loc;
+    }
+
+    // Convert Location to string
+    public static String serialize(Location loc){
+        return loc.getWorld().getName()+":"+loc.getBlockX()+":"+loc.getBlockY()+":"+loc.getBlockZ() + ":" + loc.getPitch() + ":" + loc.getYaw();
+    }
+}
