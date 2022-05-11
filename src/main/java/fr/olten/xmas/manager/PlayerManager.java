55367261095ea4814e2045d3049002e8eb9bcc05
Azodox_ <likemoi99@gmail.com>
2022-05-11 16:08:32 +0200

    Spawning particle and teleporting player when joining from survival.

--- src/main/java/fr/olten/xmas/manager/PlayerManager.java
+++ src/main/java/fr/olten/xmas/manager/PlayerManager.java
@@ -1,5 +1,6 @@
 package fr.olten.xmas.manager;
 
+import fr.mrmicky.fastparticles.ParticleType;
 import fr.olten.xmas.Lobby;
 import fr.olten.xmas.utils.LocationSerialization;
 import org.bukkit.entity.Player;
@@ -15,7 +16,35 @@ public class PlayerManager {
     }
 
     public void joiningPlayer(Player player){
-        var spawn = LocationSerialization.deserialize(Objects.requireNonNull(lobby.getConfig().getString("spawn")));
-        player.teleport(spawn);
+        if(!player.hasPlayedBefore()){
+            var spawn = LocationSerialization.deserialize(Objects.requireNonNull(lobby.getConfig().getString("spawn")));
+            player.teleport(spawn);
+        }
+    }
+
+    public void joiningPlayerFromSurvival(Player player){
+        var magic = ParticleType.of("SPELL_WITCH");
+        var survivalPortalSpawnLocation = LocationSerialization.deserialize(Objects.requireNonNull(lobby.getConfig().getString("survivalSpawnPortal")));
+
+        var particleLocation = survivalPortalSpawnLocation.clone();
+        /*
+            Credits to Finnbon (https://linktr.ee/finnbon)
+         */
+        var task = lobby.getServer().getScheduler().runTaskTimer(lobby, () -> {
+            for (double i = 0; i <= Math.PI; i += Math.PI / 15) { // 10 being the amount of circles.
+                double radius = Math.sin(i); // we get the current radius
+                double y = Math.cos(i); // we get the current y value.
+                for (double a = 0; a < Math.PI * 2; a += Math.PI / 10) {
+                    double x = Math.cos(a) * radius; // x-coordinate
+                    double z = Math.sin(a) * radius; // z-coordinate
+                    particleLocation.add(x, y /* from the previous code */, z); // 'location' is our center.
+                    magic.spawn(particleLocation.getWorld(), particleLocation, 1, 0, 0, 0, 0);
+                    particleLocation.subtract(x, y, z);
+                }
+            }
+        }, 0, 1);
+
+        lobby.getServer().getScheduler().runTaskLater(lobby, task::cancel, 20 * 2);
+        player.teleport(survivalPortalSpawnLocation);
     }
 }
