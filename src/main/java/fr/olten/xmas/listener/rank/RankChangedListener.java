a6ad5940e60523e682edd4b99d54f455464067f3
Azodox_ <likemoi99@gmail.com>
2022-05-27 16:52:40 +0200

    Cast to AccountManager and import moved class MajorRankChangedEvent (see https://github.com/Azodox/account-system/commit/5db444c61bce4482e5db13325382752b5edada81)

--- src/main/java/fr/olten/xmas/listener/rank/RankChangedListener.java
+++ src/main/java/fr/olten/xmas/listener/rank/RankChangedListener.java
@@ -1,13 +1,14 @@
 package fr.olten.xmas.listener.rank;
 
 import fr.olten.xmas.manager.TeamNameTagManager;
-import net.valneas.account.api.events.rank.MajorRankChangedEvent;
+import net.valneas.account.AccountManager;
+import net.valneas.account.events.rank.MajorRankChangedEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 public class RankChangedListener implements Listener {
 
     @EventHandler
     public void onRankChange(MajorRankChangedEvent event) {
-        TeamNameTagManager.update(event.getAccount());
+        TeamNameTagManager.update((AccountManager) event.getAccount());
     }
 }
