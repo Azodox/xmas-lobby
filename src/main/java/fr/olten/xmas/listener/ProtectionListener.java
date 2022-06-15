da88540997711a13ba69989ce47d06ae3502c48e
Azodox_ <likemoi99@gmail.com>
2022-06-16 01:37:12 +0200

    Prevents player from taking a book from a lectern and interact with item frames.

--- src/main/java/fr/olten/xmas/listener/ProtectionListener.java
+++ src/main/java/fr/olten/xmas/listener/ProtectionListener.java
@@ -1,22 +1,32 @@
 package fr.olten.xmas.listener;
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
+import io.papermc.paper.event.player.PlayerItemFrameChangeEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerTakeLecternBookEvent;
 
 public class ProtectionListener implements Listener {
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockBreak(BlockBreakEvent event){
         event.setCancelled(!event.getPlayer().hasPermission("xmas.protect.break"));
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockPlace(BlockPlaceEvent event){
         event.setCancelled(!event.getPlayer().hasPermission("xmas.protect.place"));
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onBookTaken(PlayerTakeLecternBookEvent event){
+        event.setCancelled(!event.getPlayer().hasPermission("xmas.protect.book"));
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onItemFrameChange(PlayerItemFrameChangeEvent event){
+        event.setCancelled(!event.getPlayer().hasPermission("xmas.protect.itemframe"));
     }
 }
